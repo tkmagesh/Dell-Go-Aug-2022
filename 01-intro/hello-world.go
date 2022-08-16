68fb3b339328baf952be380719cdb5dec6114678
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-16 12:46:18 +0530

    hello world in go & creating builds

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,16 @@
+/* package declaration */
+package main
+
+/* import dependency packages */
+import "fmt"
+
+/* package level declarations */
+
+/* package init function */
+
+/* main function */
+func main() {
+	fmt.Println("Welcome to Golang!")
+}
+
+/* other functions */
