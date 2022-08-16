c2d35261e4012585f16627fefed8af090109cfb1
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-16 16:27:39 +0530

    assignment-02 problem statement

---
+++ 02-programming-constructs/assignment-02.go
@@ -0,0 +1,19 @@
+/*
+	Display the following menu
+	1. Add
+	2. Subtract
+	3. Multiply
+	4. Divide
+	5. Exit
+
+	accept the menu choice from the user
+	if menu choice == 5 then exit the application
+	if menu choice < 1 or > 5 then
+		print "invalid choice"
+		display the menu again
+	if menu choice >= 1 and <= 4 then
+		accept 2 numbers from the user
+		perform the respective operation
+		print the result
+		display the menu again
+*/
