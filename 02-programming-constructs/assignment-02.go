5fb5f39d71039d5d0d6704ff5127f2193413fa47
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-16 16:45:00 +0530

    assignment-02 solution

--- 02-programming-constructs/assignment-02.go
+++ 02-programming-constructs/assignment-02.go
@@ -17,3 +17,40 @@
 		print the result
 		display the menu again
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var menu_choice, n1, n2, result int
+	for {
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+		fmt.Println("Enter the choice :")
+		fmt.Scanln(&menu_choice)
+		if menu_choice == 5 {
+			break
+		}
+		if menu_choice < 1 || menu_choice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		fmt.Println("Enter the operands :")
+		fmt.Scanln(&n1, &n2)
+		switch menu_choice {
+		case 1:
+			result = n1 + n2
+		case 2:
+			result = n1 - n2
+		case 3:
+			result = n1 * n2
+		case 4:
+			result = n1 / n2
+		}
+		fmt.Println("Result = ", result)
+	}
+}
