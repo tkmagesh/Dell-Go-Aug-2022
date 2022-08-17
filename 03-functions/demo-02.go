0f4f22c32d1b2240a25f80e714ab7f711ad30650
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-17 09:15:36 +0530

    anonymous functions

---
+++ 03-functions/demo-02.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* anonymous functions */
+	func() {
+		fmt.Println("fn invoked")
+	}()
+
+	/* with arguments */
+	func(userName string) {
+		fmt.Printf("Hi %s, Have a nice day!\n", userName)
+	}("Magesh")
+
+	/* with return values */
+	addResult := func(x, y int) int {
+		return x + y
+	}(100, 200)
+	fmt.Println("addResult = ", addResult)
+
+	subResult := func(x, y int) int {
+		return x - y
+	}(100, 200)
+	fmt.Println("subResult = ", subResult)
+
+	/* with multiple return values */
+	q, r := func(x, y int) (quotient, remainder int) {
+		quotient, remainder = x/y, x%y
+		return
+	}(100, 7)
+	fmt.Println(q, r)
+}
