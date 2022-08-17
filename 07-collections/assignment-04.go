0c473edbc7ddc58cc8c729f553e080e38b4f8661
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-17 15:08:22 +0530

    assignments 4 & 5 - solutions

--- 07-collections/assignment-04.go
+++ 07-collections/assignment-04.go
@@ -5,14 +5,31 @@
 
 package main
 
+import "fmt"
+
 func main() {
-	/* accept the inputs from the user */
+	var start, end int
+	fmt.Println("Enter the start and end :")
+	fmt.Scanln(&start, &end)
+	primes := generate_primes(start, end)
+	fmt.Println(primes)
 }
 
-func generate_primes( /*  */ ) /*  */ {
-
+func generate_primes(start, end int) []int {
+	primes := []int{}
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			primes = append(primes, no)
+		}
+	}
+	return primes
 }
 
-func isPrime( /*  */ ) /*  */ {
-
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
