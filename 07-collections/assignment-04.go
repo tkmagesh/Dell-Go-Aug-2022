532e9c4a3e79607c8c188ba3b11f24fe064a54c0
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-17 14:33:32 +0530

    assignments 4 & 5 - problem statement

---
+++ 07-collections/assignment-04.go
@@ -0,0 +1,18 @@
+/*
+	Write a program that will generate the prime numbers between start & end
+	where start & end is accepted as input from the user
+*/
+
+package main
+
+func main() {
+	/* accept the inputs from the user */
+}
+
+func generate_primes( /*  */ ) /*  */ {
+
+}
+
+func isPrime( /*  */ ) /*  */ {
+
+}
