76dcc663a81b826d651da4027f67a5f478cb8b44
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-17 12:08:06 +0530

    converting the panic to an error

---
+++ 06-panic-recovery/program-2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var DivideByZeroError = errors.New("divisor cannot be zero")
+
+func main() {
+
+	quotient, remainder, err := divide(100, 0)
+	if err != nil {
+		fmt.Println("dont attempt to divide by zero")
+	} else {
+		fmt.Println(quotient, remainder)
+	}
+}
+
+func divide(x, y int) (quotient, remainder int, err error) {
+	defer func() {
+		e := recover()
+		if e != nil {
+			err = e.(error)
+		}
+	}()
+	quotient, remainder = x/y, x%y
+	return
+}
