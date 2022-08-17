145568bc4ede02cad345651fd8b02f3381f89f34
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-17 10:12:51 +0530

    profile wrapper

--- 03-functions/demo-05.go
+++ 03-functions/demo-05.go
@@ -2,7 +2,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	/*
@@ -18,11 +21,27 @@ func main() {
 		logOperation(100, 200, subtract)
 	*/
 
-	logAdd := getLogOperation(add)
-	logAdd(100, 200)
+	/*
+		logAdd := getLogOperation(add)
+		logAdd(100, 200)
 
-	logSubtract := getLogOperation(subtract)
-	logSubtract(100, 200)
+		logSubtract := getLogOperation(subtract)
+		logSubtract(100, 200)
+	*/
+
+	/*
+		profiledAdd := profileOperation(add)
+		profiledAdd(100, 200)
+
+		profiledSubtract := profileOperation(subtract)
+		profiledSubtract(100, 200)
+	*/
+
+	log_profile_add := profileOperation(getLogOperation(add))
+	log_profile_subtract := profileOperation(getLogOperation(subtract))
+
+	log_profile_add(100, 200)
+	log_profile_subtract(100, 200)
 }
 
 func getLogOperation(operation func(int, int)) func(int, int) {
@@ -30,6 +49,15 @@ func getLogOperation(operation func(int, int)) func(int, int) {
 		fmt.Println("invocation started")
 		operation(x, y)
 		fmt.Println("invocation completed")
+	}
+}
+
+func profileOperation(operation func(int, int)) func(int, int) {
+	return func(x, y int) {
+		start := time.Now()
+		operation(x, y)
+		elapsed := time.Now().Sub(start)
+		fmt.Println("Time taken : ", elapsed)
 	}
 }
 
@@ -41,10 +69,12 @@ func getLogOperation(operation func(int, int)) func(int, int) {
 
 /* 3rd party library functions (cannot change them) */
 func add(x, y int) {
+	time.Sleep(3 * time.Second)
 	fmt.Println("add result = ", x+y)
 }
 
 func subtract(x, y int) {
+	time.Sleep(5 * time.Second)
 	fmt.Println("subtract result = ", x-y)
 }
 
