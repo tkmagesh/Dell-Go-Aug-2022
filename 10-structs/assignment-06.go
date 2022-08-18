84c4621393eb14629675f5a84694b7c75314b9b3
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-18 12:00:24 +0530

    assignment-06 problem statement

---
+++ 10-structs/assignment-06.go
@@ -0,0 +1,10 @@
+/*
+Create the following functions for the Product
+	Format() => return a string representation for the given product
+		ex: 'Id=100, Name="Grapes", Cost=50, Units=2, Category="Fruits"
+	ApplyDiscount()
+		to apply a discount to the given product
+
+Create the above functions for PerishableProduct as well
+
+*/
