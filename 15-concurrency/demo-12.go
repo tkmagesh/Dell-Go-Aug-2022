a04655e3f0a3c7d0d796434fd4229ca0a5522205
Magesh Kuppan <tkmagesh77@gmail.com>
2022-08-19 11:56:20 +0530

    more on channels

--- 15-concurrency/demo-12.go
+++ 15-concurrency/demo-12.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 func main() {
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
 	ch := make(chan int)
-	go add(100, 200, wg, ch)
+	go add(100, 200, ch)
 	result := <-ch
-	wg.Wait()
 	fmt.Println(result)
 }
 
-func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+func add(x, y int, ch chan int) {
 	time.Sleep(5 * time.Second)
 	result := x + y
 	ch <- result
-	wg.Done()
 }
